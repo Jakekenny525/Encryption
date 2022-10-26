f4fcf3878b23399ef4c038168f9198613801a794
Jake Kenny <jakenny@asu.edu>
2022-10-26 09:37:04 -0700

    Update encryption.h

--- encryption.h
+++ encryption.h
@@ -6,6 +6,8 @@
 
 char* encrypt_message(int method);
 
+//Shift the letters in the message by n (e.g. shifting by 1 turns "A" to "B")
+//Providing a negative value will shift the letters backwards (e.g. shifting by -1 turns "A" to "Z")
 char* caesar(char* message);
 
 #endif
